498373b3b5eef67c9cc6625b4278be5eebe49ce0
Wouch <wouch@noreply.xyz>
2024-06-04 16:41:12 +0300

    Task intefrace

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,1 +1,11 @@
-export interface Task {}
+import { Timestamp } from 'firebase/firestore/lite';
+
+export interface Task {
+  name: string;
+  description: string;
+  importance: number;
+  deadline: Timestamp;
+  active: boolean;
+  status: boolean;
+  uuid: string;
+}
