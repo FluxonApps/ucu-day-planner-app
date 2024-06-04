d2c2f759404a6f224cf2270ba72f5f7bfd03dcb8
Wouch <wouch@noreply.xyz>
2024-06-04 17:50:23 +0300

    fetch tasks is ready

--- src/hooks/useFetchTaskList.ts
+++ src/hooks/useFetchTaskList.ts
@@ -1,4 +1,26 @@
+import { getAuth } from 'firebase/auth';
+import { Query, addDoc, collection, query, where } from 'firebase/firestore';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { Task } from '../models/Task';
+
+import { db } from '../../firebase.config';
+
+const auth = getAuth();
+
 export function useFetchTaskList() {
-  // Here goes the functionality of fetching tasks from Firestore.
-  return [];
+  const [user] = useAuthState(auth);
+  const tasksCollectionRef = collection(db, 'tasks');
+  const userTasksCollectionQuery = user && query(tasksCollectionRef, where('uuid', '==', user.uid));
+
+  const [tasksSnapshot] = useCollection(userTasksCollectionQuery as Query<Task>);
+
+  var tasksList = tasksSnapshot?.docs?.map((taskDoc) => {
+    const task = taskDoc.data();
+
+    return task;
+  });
+  tasksList?.sort((a, b) => b.importance - a.importance);
+
+  return tasksList;
 }
