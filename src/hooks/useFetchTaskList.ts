6bf464392f434d112e85c8e91bf6c04bc6c10d5e
Wouch <wouch@noreply.xyz>
2024-06-05 12:55:53 +0300

    slight improvements to adding and changing task info

--- src/hooks/useFetchTaskList.ts
+++ src/hooks/useFetchTaskList.ts
@@ -5,21 +5,29 @@ import { useCollection } from 'react-firebase-hooks/firestore';
 import { Task } from '../models/Task';
 
 import { db } from '../../firebase.config';
+import { useMemo } from 'react';
 
 const auth = getAuth();
 
 export function useFetchTaskList() {
   const [user] = useAuthState(auth);
+
   const tasksCollectionRef = collection(db, 'tasks');
+
   const userTasksCollectionQuery = user && query(tasksCollectionRef, where('uuid', '==', user.uid));
 
   const [tasksSnapshot] = useCollection(userTasksCollectionQuery as Query<Task>);
 
-  var tasksList = tasksSnapshot?.docs?.map((taskDoc) => {
-    const task = taskDoc.data();
+  var tasksList = useMemo(
+    () =>
+      tasksSnapshot?.docs?.map((taskDoc) => {
+        const task = taskDoc.data();
+        task.ref = taskDoc.ref;
 
-    return task;
-  });
+        return task;
+      }),
+    [tasksSnapshot],
+  );
   tasksList?.sort((a, b) => b.importance - a.importance);
 
   return tasksList;
--- src/pages/TasksDemo.tsx
+++ src/pages/TasksDemo.tsx
@@ -1,11 +1,16 @@
-
 import { Box, Button, Flex, Heading, Input, Stack, HStack, Spinner } from '@chakra-ui/react';
-import { collection, addDoc, updateDoc, deleteDoc, doc, query, CollectionReference, Timestamp, DocumentReference } from 'firebase/firestore';
+import { collection, addDoc, updateDoc, deleteDoc, doc, Timestamp, DocumentReference } from 'firebase/firestore';
 import { useState } from 'react';
-import { useCollection } from 'react-firebase-hooks/firestore';
 import { Task } from '../models/Task';
+import { useFetchTaskList } from '../hooks/useFetchTaskList';
+
+import { getAuth } from 'firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 import { db } from '../../firebase.config';
+
+
+const auth = getAuth();
 
 export function TasksDemo() {
   const [newTask, setNewName] = useState('');
@@ -13,30 +18,31 @@ export function TasksDemo() {
   const [newDescription, setNewDescription] = useState('');
   const [newImportance, setNewImportance] = useState(0);
 
+
+  const [user] = useAuthState(auth);
+
   const tasksCollectionRef = collection(db, 'tasks');
 
-  const [tasks, tasksLoading, tasksError] = useCollection(query(tasksCollectionRef) as CollectionReference<Task>);
+  const tasks = useFetchTaskList();
 
   const createTask = async () => {
-    await addDoc(tasksCollectionRef, { name: String(newTask), deadline: newDeadline, description: newDescription, activated: true, importance: Number(newImportance), status: true, uuid: 'test' });
+    await addDoc(tasksCollectionRef, { name: String(newTask), deadline: newDeadline, description: newDescription, activated: true, importance: Number(newImportance), status: true, uuid: user?.uid });
   };
-  const updateTask = async (id: string, updatedName: string, updatedDescription: string, updatedDeadline: Timestamp) => {
-    const taskDoc = doc(db, 'tasks', id);
+  const updateTask = async (taskDoc: DocumentReference, updatedName: string, updatedDescription: string, updatedDeadline: Timestamp) => {
     const newFields = { name: updatedName, description: updatedDescription, deadline: updatedDeadline };
     await updateDoc(taskDoc, newFields);
   };
-  const deleteTask = async (id: string) => {
-    const taskDoc = doc(db, 'tasks', id);
+  const deleteTask = async (taskDoc: DocumentReference) => {
     await deleteDoc(taskDoc);
   };
 
-  if (tasksLoading) {
-    return <Spinner />;
-  }
+  // if (!tasks) {
+  // return <Spinner />;
+  // }
 
-  if (tasksError) {
-    return <Box>Error fetching Tasks</Box>;
-  }
+  // if (tasksError) {
+  // return <Box>Error fetching Tasks</Box>;
+  // }
 
   return (
     <Flex flexDir="column" gap="8" padding="6">
@@ -80,17 +86,17 @@ export function TasksDemo() {
       </Flex>
       <Flex gap="4" flexWrap="wrap">
         {tasks &&
-          tasks.docs.map((task) => (
-            <Box gap="6" border="1px" borderColor="gray.300" width="20%" px="6" py="8" key={task.id}>
-              <Heading>Name: {task.data().name}</Heading>
-              <Heading>Deadline: {task.data().deadline?.toDate().toLocaleString()}</Heading>
-              <Heading>Description: {task.data().description}</Heading>
-              <Heading>Importance: {task.data().importance}</Heading>
+          tasks.map((task: Task) => (
+            <Box gap="6" border="1px" borderColor="gray.300" width="20%" px="6" py="8" key={task.name}>
+              <Heading>Name: {task.name}</Heading>
+              <Heading>Deadline: {task.deadline?.toDate().toLocaleString()}</Heading>
+              <Heading>Description: {task.description}</Heading>
+              <Heading>Importance: {task.importance}</Heading>
               <HStack gap="4" mt="4">
-                <Button size="sm" colorScheme="green" onClick={() => updateTask(task.id, task.data().name, task.data().description, task.data().deadline)}>
+                <Button size="sm" colorScheme="green" onClick={() => updateTask(task.ref, task.name, task.description, task.deadline)}>
                   Change Task (doesn't really do anything)
                 </Button>
-                <Button size="sm" colorScheme="red" onClick={() => deleteTask(task.id)}>
+                <Button size="sm" colorScheme="red" onClick={() => deleteTask(task.ref)}>
                   Delete Task
                 </Button>
               </HStack>
